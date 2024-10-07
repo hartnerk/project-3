0077e564a4574380d39fcb4cfa103a0afaea05ba
Kyle <hartnerk@gmail.com>
2024-10-07 23:59:21 +0100

    added button to join and made sign up seprate page

--- splashpage/splash/views.py
+++ splashpage/splash/views.py
@@ -1,27 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Prospect
 from .forms import ProspectForm
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect, csrf_exempt
-
-
-def index(request):
-  if request.method == 'GET':
-    prospectform = ProspectForm(request.GET)
-    return render(request, 'index.html', {'prospectform': prospectform})
-  elif request.method == 'POST':
-    prospectform = ProspectForm(request.POST)
-    if prospectform.is_valid():
-      prospectform = Prospect(
-          name = prospectform.cleaned_data['name'],
-          email = prospectform.cleaned_data['email'],  
-          message = prospectform.cleaned_data['message']
-      )
-      prospectform.save()
-    else:
-      prospectform = ProspectForm()
-    return render(request, 'index.html', {'prospectform': prospectform})
 
 def service(request):
   template = loader.get_template('service-details.html')
@@ -29,6 +11,10 @@ def service(request):
 
 def prizes(request):
   template = loader.get_template('portfolio-details.html')
+  return HttpResponse(template.render())
+
+def index(request):
+  template = loader.get_template('index.html')
   return HttpResponse(template.render())
 
 def signup(request):
