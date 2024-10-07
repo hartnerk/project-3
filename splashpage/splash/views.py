13722756a9105a17d1e0a9c53b4e1e28df9ad97b
Kyle <hartnerk@gmail.com>
2024-10-07 21:00:50 +0100

    update to views for index page

--- splashpage/splash/views.py
+++ splashpage/splash/views.py
@@ -7,8 +7,21 @@ from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect, csrf_
 
 
 def index(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+  if request.method == 'GET':
+    prospectform = ProspectForm(request.GET)
+    return render(request, 'index.html', {'prospectform': prospectform})
+  elif request.method == 'POST':
+    prospectform = ProspectForm(request.POST)
+    if prospectform.is_valid():
+      prospectform = Prospect(
+          name = prospectform.cleaned_data['name'],
+          email = prospectform.cleaned_data['email'],  
+          message = prospectform.cleaned_data['message']
+      )
+      prospectform.save()
+    else:
+      prospectform = ProspectForm()
+    return render(request, 'index.html', {'prospectform': prospectform})
 
 def service(request):
   template = loader.get_template('service-details.html')
